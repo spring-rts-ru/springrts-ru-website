a966b3068e5984b9b9d5382b9090f39a80e19564
Alexander Lipatov <alexlpt@yandex.ru>
2015-06-19 23:06:38 +0300

    Add "spring 99" release notes

--- src/main/scala/springrtsru/pages/AboutPage.scala
+++ src/main/scala/springrtsru/pages/AboutPage.scala
@@ -3,7 +3,5 @@ package springrtsru.pages
 import org.apache.wicket.markup.html.basic.Label
 
 class AboutPage extends BasePage {
-  add(new Label("message", "Hello spring!"))
-
-  override def getTitle: String = "Все о русскоязычном сообществе Spring"
+  override def getTitle: String = "Долгожданный релиз Spring 99!"
 }

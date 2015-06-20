c18a95b00888e8baa7cf636f08a462bdaf19ed3d
Alexander Lipatov <alexlpt@yandex.ru>
2015-06-20 15:44:23 +0300

    Add logo and a favicon

---
+++ src/main/scala/springrtsru/pages/MediaPage.scala
@@ -0,0 +1,7 @@
+package springrtsru.pages
+
+import org.apache.wicket.markup.html.basic.Label
+
+class MediaPage extends BasePage {
+  override def getTitle: String = "Скриншоты из различных игр, основанных на движке Spring"
+}

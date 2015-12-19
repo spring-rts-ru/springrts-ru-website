7936c193f688fdf196406876297a2d1b67da17ed
Alexander Lipatov <alexlpt@yandex.ru>
2015-12-19 23:47:54 +0300

    Wicket related changed for 1944 gane

---
+++ src/main/scala/springrtsru/pages/games/I944GamePage.scala
@@ -0,0 +1,9 @@
+package springrtsru.pages.games
+
+import springrtsru.pages.{Games, PageIndex, BasePage}
+
+class I944GamePage extends BasePage {
+  override def getTitle: String = "Spring 1944 RTS"
+
+  override def getPageIndex: PageIndex = Games
+}

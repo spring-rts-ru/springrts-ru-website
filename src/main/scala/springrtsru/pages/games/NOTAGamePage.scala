46d6be049273f7e5bc8a63f494d73a8f24ef89ac
Elt <rgetmanzev@gmail.com>
2015-12-23 12:15:42 +0300

    Fixed typo in NOTAGamePage: name of page changed to "NOTA"

--- src/main/scala/springrtsru/pages/games/NOTAGamePage.scala
+++ src/main/scala/springrtsru/pages/games/NOTAGamePage.scala
@@ -3,7 +3,7 @@ package springrtsru.pages.games
 import springrtsru.pages.{Games, PageIndex, BasePage}
 
 class NOTAGamePage extends BasePage {
-  override def getTitle: String = "TOTA"
+  override def getTitle: String = "NOTA"
 
   override def getPageIndex: PageIndex = Games
 }
